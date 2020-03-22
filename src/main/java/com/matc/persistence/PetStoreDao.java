80fc8614cf65b1ea7d86ad7fcaff67e2c593eace
kpalese <kelly.palese@gmail.com>
2020-03-22 14:27:45 -0500

    Adding Pet class and starting PetStoreDao

---
+++ src/main/java/com/matc/persistence/PetStoreDao.java
@@ -0,0 +1,31 @@
+package com.matc.persistence;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.petstore.Pet;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+public class PetStoreDao {
+
+    //Probably would have a parameter passed in to get the correct planet ID rather than just getting the 1st id
+    Pet getPet() {
+        Client client = ClientBuilder.newClient();
+        //TODO: Read in the uri from a properties file
+        WebTarget target =
+                client.target("https://swapi.co/api/planets/1");
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        Pet pet = null;
+        try {
+            pet = mapper.readValue(response, Pet.class);
+        } catch (JsonProcessingException e) {
+            //TODO: Set up logging and write this to the log
+            e.printStackTrace();
+        }
+        return pet;
+    }
+}
---
+++ src/main/java/com/petstore/Pet.java
@@ -0,0 +1,10 @@
+package com.petstore;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.annotation.Generated;
+import javax.annotation.processing.Generated;
+
+@Generated("com.robohorse.robopojogenerator")
+
+public class Pet {
+}
